647414d460a02919728d5cc507a23d2e1525b941
Robin Deits <robin.deits@gmail.com>
2016-07-11 17:16:06 -0400

    initial commit

---
+++ ac-remote-server/src/main.rs
@@ -0,0 +1,27 @@
+extern crate rustc_serialize;
+#[macro_use] extern crate nickel;
+
+use nickel::{Nickel, HttpRouter, JsonBody};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Person {
+	firstname: String, 
+	lastname: String,
+}
+
+fn main() {
+	let mut server = Nickel::new();
+
+	server.post("/a/post/request", middleware! { |request, response|
+		let person = request.json_as::<Person>().unwrap();
+		format!("Hello {} {}", person.firstname, person.lastname)
+	});
+
+	// server.utilize(router! {
+	// 	get "**" => |_req, _res| {
+	// 		"Hello world!"
+	// 	}
+	// });
+
+	server.listen("127.0.0.1:6767");
+}
